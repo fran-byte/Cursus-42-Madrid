d169fbd87e7d504d0be304dae7cf9048df7eca42
fran-byte <fran-byte@hotmail.com>
2024-09-17 00:39:30 +0200

    up

--- projects/libft/ft_strncmp.c
+++ projects/libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 22:46:06 by frromero          #+#    #+#             */
-/*   Updated: 2024/09/17 00:07:05 by frromero         ###   ########.fr       */
+/*   Updated: 2024/09/17 00:39:23 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,33 +18,4 @@ int ft_strncmp(const char *s1, const char *s2, unsigned int n)
     while(s1[i] == s2[i] && i < n)      
         i++;
     return (s1[i] - s2[i]);
-}
-
-int main()
-{
-    // s1 vacía, s2 vacía
-    printf("Ejemplo 1: %d\n", ft_strncmp("", "", 4)); 
-    //  0 ('\0' - '\0' = 0)
-
-    // s1 vacía, s2 no vacía
-    printf("Ejemplo 1: %d\n", ft_strncmp("", "abc", 4)); 
-    //  -97 ('\0' - 'a' = -97)
-
-    // s1 más larga que s2, n mayor que longitud de s2
-    printf("Ejemplo 2: %d\n", ft_strncmp("Hello", "Hell", 10)); 
-    //  111 ('o' - '\0' = 111)
-
-    // s1 más corta que s2, n mayor que longitud de s1
-    printf("Ejemplo 3: %d\n", ft_strncmp("Hi", "Hello", 10)); 
-    //  4 ('i' - 'e' = 105 - 101 = 4)
-
-    // s1 y s2 son idénticas hasta el límite n
-    printf("Ejemplo 4: %d\n", ft_strncmp("HelloWorld", "Hello", 10)); 
-    //  87 ('W' - '\0' = 87)
-
-    // s1 y s2 son iguales hasta n, pero n es mayor que las longitudes
-    printf("Ejemplo 5: %d\n", ft_strncmp("Hi", "Hi", 10)); 
-    // 0 (porque las cadenas son iguales y n es mayor que la longitud de las cadenas)
-
-    return 0;
 }
--- projects/libft/libft.h
+++ projects/libft/libft.h
@@ -6,27 +6,31 @@
 /*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/13 21:00:56 by frromero          #+#    #+#             */
-/*   Updated: 2024/09/16 23:49:51 by frromero         ###   ########.fr       */
+/*   Updated: 2024/09/17 00:29:46 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int ft_isalpha(char c);
-int ft_isalnum(char c);
-int ft_isdigit(char c);
-int ft_isascii(char c);
-int ft_isprint(char c);
-int ft_strlen (char *str);
-unsigned int ft_strlcpy(char *dst, const char *src, unsigned int size);
-unsigned int ft_strlcat(char *dst, const char *src, unsigned int size);
+unsigned int ft_strlen(const char *str);
+void *ft_memset(void *s, int c, unsigned int n);
+void ft_bzero(void *s, unsigned int n);
+void *ft_memcpy(void *dest, const void *src, unsigned int n);
+void *ft_memmove(void *dest, const void *src, unsigned int n);
+unsigned int ft_strlcpy(char *dst, const char *src, unsigned int dstsize);
+unsigned int ft_strlcat(char *dst, const char *src, unsigned int dstsize);
+int ft_toupper(int c);
+int ft_tolower(int c);
 char *ft_strchr(const char *s, int c);
 char *ft_strrchr(const char *s, int c);
-
 int ft_strncmp(const char *s1, const char *s2, unsigned int n);
-int ft_toupper(char c);
-int ft_tolower(char c);
+void *ft_memchr(const void *s, int c, unsigned int n);
+int ft_memcmp(const void *s1, const void *s2, unsigned int n);
+char *ft_strnstr(const char *haystack, const char *needle, unsigned int len);
+int ft_atoi(const char *str);
+char *ft_strdup(const char *s1);
+void *ft_calloc(unsigned int count, unsigned int size);
 
 
 #endif
