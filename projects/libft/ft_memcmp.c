ffb2fffceca757d37dcabe5c4c3f7757adcc9573
fran-byte <fran-byte@hotmail.com>
2024-09-22 20:00:27 +0200

    UP

--- projects/libft/ft_memcmp.c
+++ projects/libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 11:21:02 by frromero          #+#    #+#             */
-/*   Updated: 2024/09/22 08:43:35 by frromero         ###   ########.fr       */
+/*   Updated: 2024/09/22 19:58:05 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
+
+	str1 = (unsigned char *)s1;
+	str2 = (unsigned char *)s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }
