ff3d74ed085d65dbf6f162179c7e8e83630edcd5
fran-byte <fran-byte@hotmail.com>
2024-09-23 18:41:34 +0200

    Update ft_strnstr.c

--- projects/libft/ft_strnstr.c
+++ projects/libft/ft_strnstr.c
@@ -14,4 +14,24 @@
 
 char	*ft_strnstr(const char *big, const char *little, size_t len)
 {
+	size_t	b;
+	size_t	l;
+
+	b = 0;
+	if (*little == '\0')
+		return ((char *)big);
+	while(big[b] != '\0')
+	{
+		l = 0;
+		while(big[b + l] == little[l] && (b + l < len))
+		{
+			if (big[b + l] == '\0' && little[l] == '\0')
+				return ((char *)&big[b]);
+			l++;
+		}
+		if (little[b + l] == '\0')
+			return ((char *)big + b);
+		b++;
+	}
+	return (0);
 }
