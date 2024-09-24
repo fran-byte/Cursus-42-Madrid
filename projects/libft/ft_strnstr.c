e66f3853513197bd13c03cee4664ca44eab54adb
fran-byte <fran-byte@hotmail.com>
2024-09-24 12:33:59 +0200

    Update ft_strnstr.c

--- projects/libft/ft_strnstr.c
+++ projects/libft/ft_strnstr.c
@@ -15,25 +15,25 @@
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char tiny, size_t len)
+char	*ft_strnstr(const char *big, const char *tiny, size_t len)
 {
 	size_t	b;
 	size_t	t;
 
 	b = 0;
-	if (tiny == '\0')
+	if (tiny[0] == '\0')
 		return ((char *)big);
 	while(big[b] != '\0')
 	{
 		t = 0;
-		while(big[b + t] == tiny[t] && (b + t < len))
+		while(big[b + t] == tiny[t] && (b + t) < len)
 		{
 			if (big[b + t] == '\0' && tiny[t] == '\0')
 				return ((char *)&big[b]);
 			t++;
 		}
-		if (tiny[b + t] == '\0')
-			return ((char *)big + b);
+		if (tiny[t] == '\0')
+			return ((char *)big[b]);
 		b++;
 	}
 	return (0);
