3545dee0e1f3e8ec9b054995666105d1e7d9cc94
fran-byte <fran-byte@hotmail.com>
2024-09-27 09:38:31 +0200

    Update ft_strtrim.c

--- projects/libft/ft_strtrim.c
+++ projects/libft/ft_strtrim.c
@@ -14,13 +14,25 @@
 
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	char	*s1_cp;
-	char	*set_cp;
 	char	*trim;
+	size_t	len;
+	size_t	i;
 
-	s1_cp = s1;
-	set_cp = set;
-
-
-
+	if (!s1)
+		return (NULL);
+	if (!set)
+		return (ft_strdup(s1));
+	i = 0;
+	while (s1[i] && ft_strchr(set, s1[i]))
+		i++;
+	if (i == ft_strlen(s1))
+		return (ft_strdup(""));
+	len = ft_strlen(s1);
+	while (s1[len - 1] && ft_strchr(set, s1[len - 1]))
+		len--;
+	trim = malloc(sizeof(char) * (len - i + 1));
+	if (!trim)
+		return (NULL);
+	ft_strlcpy(trim, s1 + i, len - i + 1);
+	return (trim);
 }
