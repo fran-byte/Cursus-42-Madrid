5d00273816f6d57dc6f42ce746d00a283c46050f
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:10:31 +0200

    Update ft_isprint.c

--- projects/libft/ft_isprint.c
+++ projects/libft/ft_isprint.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:29:23 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/22 08:35:13 by frromero         ###   ########.fr       */
