089869f7309ef0e3481eb0eea301355ffdc106f5
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:14:10 +0200

    Update ft_lstsize.c

--- projects/libft/ft_lstsize.c
+++ projects/libft/ft_lstsize.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstsize.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:57:32 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 15:57:34 by frromero         ###   ########.fr       */
