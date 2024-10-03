7fa0d71f58596859ef839d4f26e4beebc1ddd59b
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:12:25 +0200

    Update ft_lstdelone.c

--- projects/libft/ft_lstdelone.c
+++ projects/libft/ft_lstdelone.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstdelone.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:56:45 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 15:56:48 by frromero         ###   ########.fr       */
