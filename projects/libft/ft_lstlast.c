6df8a1214f7778fa20c41094393ab06c0436d435
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:13:09 +0200

    Update ft_lstlast.c

--- projects/libft/ft_lstlast.c
+++ projects/libft/ft_lstlast.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstlast.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:57:07 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 15:57:10 by frromero         ###   ########.fr       */
