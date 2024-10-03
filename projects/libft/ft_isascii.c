955e5e4fb4d810edd8eb3a35ce6f8cd86ad97b58
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:09:39 +0200

    Update ft_isascii.c

--- projects/libft/ft_isascii.c
+++ projects/libft/ft_isascii.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:23:03 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/22 08:34:45 by frromero         ###   ########.fr       */
