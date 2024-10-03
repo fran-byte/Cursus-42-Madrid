08d9e8268a8d640a5b2c5690e65b03d21512f1a8
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:12:49 +0200

    Update ft_lstiter.c

--- projects/libft/ft_lstiter.c
+++ projects/libft/ft_lstiter.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstiter.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:56:52 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 15:56:54 by frromero         ###   ########.fr       */
