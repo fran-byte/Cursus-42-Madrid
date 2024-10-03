a9d2a7ceacae86cec1d9ac2b847a8dbf7bb001e0
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:21:00 +0200

    Update ft_strlen.c

--- projects/libft/ft_strlen.c
+++ projects/libft/ft_strlen.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlen.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 10:31:41 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/26 17:21:25 by frromero         ###   ########.fr       */
