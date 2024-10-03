3626f53f344012bb8c3bb2b67d7d9fce15d5f226
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:16:37 +0200

    Update ft_putchar_fd.c

--- projects/libft/ft_putchar_fd.c
+++ projects/libft/ft_putchar_fd.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_putchar_fd.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 15:59:33 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 15:59:35 by frromero         ###   ########.fr       */
