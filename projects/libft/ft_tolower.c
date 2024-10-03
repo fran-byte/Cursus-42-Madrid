4555792092236b75b9ca50ea5061c8b6bcfea660
fran-byte <fran-byte@hotmail.com>
2024-10-03 09:31:48 +0200

    Update ft_tolower.c

--- projects/libft/ft_tolower.c
+++ projects/libft/ft_tolower.c
@@ -3,7 +3,7 @@
 /*                                                        :::      ::::::::   */
 /*   ft_tolower.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: frromero <frromero@student.42.fr>          +#+  +:+       +#+        */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/14 22:42:52 by frromero          #+#    #+#             */
 /*   Updated: 2024/09/30 18:21:48 by frromero         ###   ########.fr       */
