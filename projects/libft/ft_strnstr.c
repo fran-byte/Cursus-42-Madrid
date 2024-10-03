c4932d3a172a692838737997843524f7b2bc8c07
fran-byte <fran-byte@hotmail.com>
2024-10-03 21:54:16 +0200

    up

--- projects/libft/ft_strnstr.c
+++ projects/libft/ft_strnstr.c
@@ -6,37 +6,37 @@
 /*   By: frromero <frromero@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 18:36:53 by frromero          #+#    #+#             */
-/*   Updated: 2024/10/03 21:20:52 by frromero         ###   ########.fr       */
+/*   Updated: 2024/10/03 21:54:10 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/* This function locates the first occurrence of the substring `tiny` in the
- * string `big`, searching within the first `len` characters of `big`. If `tiny`
- * is an empty string, it returns `big`. It returns a pointer to the start of
+/* This function locates the first occurrence of the substring `substr` in the
+ * string `src`, searching within the first `len` characters of `src`. If `substr`
+ * is an empty string, it returns `src`. It returns a pointer to the start of
  * the located substring, or NULL if the substring is not found within the
  * specified length.*/
 
 #include "libft.h"
 
-char	*ft_strnstr(const char *big, const char *tiny, size_t len)
+char	*ft_strnstr(const char *src, const char *substr, size_t len)
 {
 	size_t	b;
 	size_t	l;
 
 	b = 0;
-	if (*tiny == '\0')
-		return ((char *)big);
-	while (big[b] != '\0')
+	if (*substr == '\0')
+		return ((char *)src);
+	while (src[b] != '\0')
 	{
 		l = 0;
-		while (big[b + l] == tiny[l] && (b + l) < len)
+		while (src[b + l] == substr[l] && (b + l) < len)
 		{
-			if (big[b + l] == '\0' && tiny[l] == '\0')
-				return ((char *)&big[b]);
+			if (src[b + l] == '\0' && substr[l] == '\0')
+				return ((char *)&src[b]);
 			l++;
 		}
-		if (tiny[l] == '\0')
-			return ((char *)big + b);
+		if (substr[l] == '\0')
+			return ((char *)src + b);
 		b++;
 	}
 	return (0);
