628a14c3bca11c54f0f0d0709049b2ae77f3b7f3
fran-byte <fran-byte@hotmail.com>
2024-11-01 19:22:32 +0100

    Update get_next_line.c

--- milestone_1/getnextline in progress/get_next_line.c
+++ milestone_1/getnextline in progress/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/22 14:35:46 by frromero          #+#    #+#             */
-/*   Updated: 2024/11/01 00:02:04 by frromero         ###   ########.fr       */
+/*   Updated: 2024/11/01 19:21:03 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,17 +15,17 @@
 char	*find_and_return_line(char *stored_bff)
 {
 	int	i;
-	char *line;
-	int ln;
+	char	*line;
+	int	ln;
 
 	i = 0;
 	ln = 0;
 	while (stored_bff[ln] != '\n' && stored_bff[ln] != '\0') //calculamos longitud de line
 		ln++;
-	while(*stored_bff)
+	while (*stored_bff)
 	{
-		line = (char *)malloc(ln * sizeof(char)+1); // reservamos tamaño de line
-		if (!line)
+		line = (char *)malloc(ln * sizeof(char) + 1); // reservamos tamaño de line
+		if (line == NULL)
 			return (NULL);
 		i = -1;
 		while (++i <= ln)
@@ -35,25 +35,27 @@ char	*find_and_return_line(char *stored_bff)
 	}
 	return (NULL);
 }
+
+
 char	*swap_delete(char *stored_bff)
 {
 	int 	i;
 	char	*swap_temp;
-	int ln;
-	int ln_total;
+	int	ln;
+	int	ln_total;
 
 	ln = 0;
 	ln_total = 0;
 	while (stored_bff[ln] != '\n') //calculamos longitud de line
-				ln++;
+		ln++;
 	ln_total = ft_strlen(stored_bff); //calculamos longitud de stored_bff
 	i = 0;
-	swap_temp = (char *)malloc(ln_total  * sizeof(char) + 1); // mismo tamaño que el restante stored_bff
-	if (!swap_temp)
+	swap_temp = (char *)malloc(ln_total * sizeof(char) + 1); // mismo tamaño que el restante stored_bff
+	if (swap_temp == NULL)
 		return (NULL);
 	while (stored_bff[ln] != '\0') // hacemos la copia de la parte restante
 	{
-		swap_temp[i] = stored_bff [ln+1];
+		swap_temp[i] = stored_bff [ln + 1];
 		i++;
 		ln++;
 	}
@@ -61,52 +63,76 @@ char	*swap_delete(char *stored_bff)
 	free (stored_bff);
 	return (swap_temp);
 }
+
 char	*read_and_join(char *stored_bff, int fd)
 {
-	int bytes_read;
-	char *read_bff;
+	int	bytes_read;
+	char	*read_bff;
 
-		read_bff = (char *)malloc((BUFFER_SIZE) * sizeof(char) + 1); // Tamaño asinado para read_bff
-		if (!read_bff)
+	read_bff = (char *)malloc((BUFFER_SIZE) * sizeof(char) + 1); // Tamaño asinado para read_bff
+	//printf("Read_bff = %p\n", read_bff);
+	if (read_bff == NULL)
+		return (NULL);
+	if (stored_bff == NULL)
+	{
+		stored_bff = (char *)malloc(1 * sizeof(char)); // Inicializar stored_bff como cadena vacía
+		if (stored_bff == NULL)
 			return (NULL);
-		if (!stored_bff)
+	}
+	bytes_read = read(fd, read_bff, BUFFER_SIZE); // LEEMOS a read_bff !!!
+	if (bytes_read <= 0)
+	{
+		free(read_bff);
+		if (stored_bff[0] == '\0') // Si no hay nada acumulado
 		{
-				stored_bff =(char *)malloc(1 * sizeof(char)); // Inicializar stored_bff como cadena vacía
-				if (!stored_bff)
-					return (NULL);
+			free(stored_bff);
+			return (NULL); // Termina la función
 		}
-		bytes_read = read(fd, read_bff, BUFFER_SIZE); // LEEMOS a read_bff !!!
-		if (bytes_read <= 0)
-		{
-			free(read_bff);
-			if (stored_bff[0] == '\0') // Si no hay nada acumulado
-			{
-                return (NULL); // Termina la función
-			}
-		}
-        read_bff[bytes_read] = '\0'; // la str de read_buff terminado en nulo
-		stored_bff = ft_strjoin(stored_bff, read_bff); // Contatenamos stored_bff+read_bff
-		free(read_bff); //liberamos el read_bff que ya no utilizamos
-		return (stored_bff);
+		free(stored_bff);
+		return (NULL); // REVISAR
+	}
+	read_bff[bytes_read] = '\0'; // la str de read_buff terminado en nulo
+	stored_bff = special_strjoin_(stored_bff, read_bff); // Contatenamos stored_bff+read_bff
+	free(read_bff); //liberamos el read_bff que ya no utilizamos
+	return (stored_bff);
 }
 
 char	*get_next_line(int fd)
 {
-	char *line;
-	static char *stored_bff;
+	char	*line;
+	static char	*stored_bff;
 
-	if (stored_bff == NULL)
-		stored_bff = NULL; // Solo se pone a NULL en la primera pasada
-	if (fd == -1)
+	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
-	if (!stored_bff) // Si el buffer acumulado está vacio entramos y llamamos a read_and_join
+	if (stored_bff == NULL) // Si el buffer acumulado no apunta a nada entramos y llamamos a read_and_join
+	{
 		stored_bff = read_and_join(stored_bff, fd); // aquí ya tenemos la cadena concatenada
-	while(ft_strchr(stored_bff, '\n') == NULL) // si el stored_buff no está vacio, pero no tiene \n
+		if (stored_bff == NULL) // pero Si el buffer acumulado no apunta:
+		{
+			free (stored_bff);
+			return (NULL);
+		}
+	}
+	while (ft_strchr(stored_bff, '\n') == NULL) // si el stored_buff no está vacio, pero no tiene \n
+	{
 		stored_bff = read_and_join(stored_bff, fd); //vamos a leer del stored_bff y concatenar hasta obtener un \n
+		if (stored_bff == NULL)
+		{
+			free (stored_bff);
+			return (NULL);
+		}
+	}
 	line = find_and_return_line(stored_bff);// vamos a filtar la stored_bff y obtener la línea
 	stored_bff = swap_delete(stored_bff);
+	if (stored_bff == NULL)
+		{
+		free (stored_bff);
+		return (NULL);
+		}
 	return (line);
 }
+
+/*
 #include <fcntl.h>
 
 int main(void) {
@@ -139,3 +165,5 @@ int main(void) {
 	close(fd);
 	return (0);
 }
+//cc -fsanitize=leak -static-libsan get_next_line.c  get_next_line_utils.c && ./a.out
+*/
