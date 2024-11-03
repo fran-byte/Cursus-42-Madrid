2ef54a6251fb135dd7ee8d83eca7431371bfb2cd
fran-byte <fran-byte@hotmail.com>
2024-11-04 00:30:00 +0100

    Update get_next_line.h

--- milestone_1/getnextline in progress/get_next_line.h
+++ milestone_1/getnextline in progress/get_next_line.h
@@ -23,7 +23,7 @@ size_t	ft_strlen(char *str);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
 size_t	ft_strlcat(char *dst, char *src, size_t size);
 char	*ft_strjoin(char *read_bff, char *stored_bff);
-char	*find_and_return_line(char *stored_bff);
+char	*special_strjoin_(char *read_bff, char *stored);
 
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
