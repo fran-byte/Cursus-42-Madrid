d02a51eda08e55aad34e60cbf2b2baba85d057d9
fran-byte <fran-byte@hotmail.com>
2024-11-06 19:54:37 +0100

    Update get_next_line.h

--- milestone_1/getnextline in progress/get_next_line.h
+++ milestone_1/getnextline in progress/get_next_line.h
@@ -6,19 +6,18 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/29 14:35:27 by frromero          #+#    #+#             */
-/*   Updated: 2024/11/05 21:46:22 by frromero         ###   ########.fr       */
+/*   Updated: 2024/11/06 19:42:27 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-
-#include <unistd.h>
-#include <stdlib.h>
-//#include <stdio.h>  // BORRAR****************************************
-#include <limits.h>
-
-//#define BUFFER_SIZE 1  // ELIMINAR *********************************
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
 
 size_t	ft_strlen(char *str);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
@@ -30,7 +29,4 @@ char	*read_join(char *stored, int fd);
 char	*re_adj_storage(char *stored);
 char	*get_next_line(int fd);
 char	*ft_strchr(const char *s, int c);
-
-//int main(void); // ELIMINAR ********************
-
 #endif
