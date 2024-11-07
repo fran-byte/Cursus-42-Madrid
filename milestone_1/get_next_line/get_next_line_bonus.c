58c2eb4a357f5ea2e99fbb926264823c66cdc801
fran-byte <fran-byte@hotmail.com>
2024-11-07 09:38:55 +0100

    Add files via upload

---
+++ milestone_1/get_next_line/get_next_line_bonus.c
@@ -0,0 +1,156 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/02 10:59:24 by frromero          #+#    #+#             */
+/*   Updated: 2024/11/07 09:13:42 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+/* This function reads from a file descriptor line by line. */
+/* It stores leftover text in a static buffer between calls, */
+/* handling partial lines across multiple reads. */
+/* The `my_line` function extracts a full line */
+
+#include "get_next_line_bonus.h"
+
+char	*my_line(char *stored)
+{
+	int			i;
+	char		*line;
+	int			ln;
+
+	i = 0;
+	ln = 0;
+	while (stored[ln] != '\n' && stored[ln] != '\0')
+		ln++;
+	if (*stored)
+	{
+		line = (char *)malloc((ln + 1) * sizeof(char) + 1);
+		if (line == NULL)
+			return (free(stored), NULL);
+		i = 0;
+		while (i <= ln)
+		{
+			line[i] = stored[i];
+			i++;
+		}
+		line[i] = '\0';
+		return (line);
+	}
+	return (free(stored), NULL);
+}
+
+char	*re_adj_storage(char *stored)
+{
+	int		i;
+	int		j;
+	char	*swap_temp;
+
+	i = 0;
+	j = 0;
+	while (stored[i] && stored[i] != '\n')
+		i++;
+	if (!stored[i++])
+	{
+		free(stored);
+		return (NULL);
+	}
+	swap_temp = (char *)malloc((ft_strlen(stored) - i + 1) * sizeof(char));
+	if (!swap_temp)
+		return (NULL);
+	while (stored[i])
+		swap_temp[j++] = stored[i++];
+	swap_temp[j] = '\0';
+	return (free(stored), swap_temp);
+}
+
+char	*initialize_stored(char *stored, char *read_bff)
+{
+	if (!stored)
+	{
+		stored = malloc(1);
+		if (!stored)
+		{
+			free(read_bff);
+			return (NULL);
+		}
+		*stored = '\0';
+	}
+	return (stored);
+}
+
+char	*read_join(char *stored, int fd)
+{
+	ssize_t	bytes_read;
+	char	*read_bff;
+
+	read_bff = malloc(BUFFER_SIZE + 1);
+	if (!read_bff)
+		return (NULL);
+	if (!stored)
+		stored = initialize_stored(stored, read_bff);
+	while (1)
+	{
+		bytes_read = read(fd, read_bff, BUFFER_SIZE);
+		if (bytes_read <= 0)
+		{
+			free(read_bff);
+			if (bytes_read == 0 && stored[0] != '\0')
+				return (stored);
+			return (free(stored), NULL);
+		}
+		read_bff[bytes_read] = '\0';
+		stored = special_strjoin_(stored, read_bff);
+		if (ft_strchr(read_bff, '\n'))
+			break ;
+	}
+	return (free(read_bff), stored);
+}
+
+char	*get_next_line(int fd)
+{
+	char			*line;
+	static char		*stored[65536];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	stored[fd] = read_join(stored[fd], fd);
+	if (!stored[fd])
+		return (free(stored[fd]), NULL);
+	line = my_line(stored[fd]);
+	stored[fd] = re_adj_storage(stored[fd]);
+	return (line);
+}
+
+/*
+#include <fcntl.h>
+#include <stdio.h>
+
+// Compilar: cc -g -fsanitize=address -o a.out get_next_line.c
+get_next_line_utils.c && ./a.out
+// Compilar si vamos a pasar valgrind: sin fsanitize
+//cc -g3 -o a.out get_next_line.c get_next_line_utils.c && ./a.out
+
+int main(void)
+{
+	char *next_line;
+	int fd = open("files/42_with_nl", O_RDONLY);
+
+	if (fd == -1)
+	{
+		printf("Error: no se pudo abrir el archivo.\n");
+		return (1);
+	}
+	while ((next_line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", next_line);
+		free(next_line);
+	}
+	free(next_line);
+	close(fd);
+	return (0);
+}*/
---
+++ milestone_1/get_next_line/get_next_line_utils.c
@@ -0,0 +1,101 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/29 14:36:13 by frromero          #+#    #+#             */
+/*   Updated: 2024/11/06 19:40:59 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	if (str == NULL)
+		return (0);
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+size_t	ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size == 0)
+		return (ft_strlen(src));
+	while (src[i] != '\0' && i < size - 1)
+	{
+		dst[i] = src[i];
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+size_t	ft_strlcat(char *dst, char *src, size_t size)
+{
+	size_t	i;
+	size_t	len_dst;
+	size_t	len_src;
+
+	i = 0;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (size == 0)
+		return (len_src);
+	if (size <= len_dst)
+		return (len_src + size);
+	while (src[i] != '\0' && (len_dst + i) < (size - 1))
+	{
+		dst[i + len_dst] = src[i];
+		i++;
+	}
+	dst[i + len_dst] = '\0';
+	return (len_src + len_dst);
+}
+
+char	*special_strjoin_(char *stored, char *read_bff)
+{
+	int		total_sz;
+	char	*str;
+
+	if (!read_bff)
+	{
+		read_bff = malloc(sizeof(char) + 1);
+		if (!read_bff)
+			return (NULL);
+		read_bff[0] = 0;
+	}
+	total_sz = ft_strlen(read_bff) + ft_strlen(stored);
+	str = (char *)malloc((sizeof(char) * total_sz) + 1);
+	if (!str)
+		return (NULL);
+	ft_strlcpy(str, stored, total_sz + 1);
+	ft_strlcat(str, read_bff, total_sz + 1);
+	free(stored);
+	return (str);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] == (char)c)
+			return ((char *)&s[i]);
+		i++;
+	}
+	if (s[i] == (char)c)
+		return ((char *)&s[i]);
+	return (NULL);
+}
