9c8db78603052aea1ac857e21f44d5558a398e9e
fran-byte <fran-byte@hotmail.com>
2024-11-16 15:42:28 +0100

    Create main.c

---
+++ milestone_1/get_next_line/test/main.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main_bonus.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/16 14:41:56 by frromero          #+#    #+#             */
+/*   Updated: 2024/11/16 15:07:43 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+#include "get_next_line.h"
+
+int main()
+{
+	printf("BUFFER_SIZE=%d\n", BUFFER_SIZE);
+
+	int fd2 = open("test2", O_RDONLY);
+
+	printf("%s\n", get_next_line(fd2));
+	printf("%s\n", get_next_line(fd2));
+	printf("%s\n", get_next_line(fd2));
+	close(fd2);
+
+	printf("BUFFER_SIZE=%d\n", BUFFER_SIZE);
+	int fd3 = open("test1", O_RDONLY);
+
+	printf("%s\n", get_next_line(fd3));
+	printf("%s\n", get_next_line(fd3));
+	printf("%s\n", get_next_line(fd3));
+	close(fd3);
+}
