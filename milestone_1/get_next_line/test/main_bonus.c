6bdcff5963c979d071e8a1f4c5deabac1cb94a08
fran-byte <fran-byte@hotmail.com>
2024-11-16 16:05:15 +0100

    Update main_bonus.c

--- milestone_1/get_next_line/test/main_bonus.c
+++ milestone_1/get_next_line/test/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/16 14:41:56 by frromero          #+#    #+#             */
-/*   Updated: 2024/11/16 15:07:43 by frromero         ###   ########.fr       */
+/*   Updated: 2024/11/16 16:04:24 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,40 +16,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "get_next_line_bonus.h"
+#include "get_next_line.h"
 
 int main()
 {
-	printf("BUFFER_SIZE=%d\n", BUFFER_SIZE);
-	char *line1 = NULL;
-	char *line2 = NULL;
-	int fd = open("test1", O_RDONLY);
+
+	char *line;
+
+	printf("\nBUFFER_SIZE=%d\n\n", BUFFER_SIZE);
+
 	int fd2 = open("test2", O_RDONLY);
-	int i = 0;
+	int fd1 = open("test1", O_RDONLY);
+	int fd0 = open("test3", O_RDONLY);
 
-
-	get_next_line(fd);
-	get_next_line(fd2);
-	printf("%s:%s\n", line1, line2);
-
-	close(fd);
+	printf("%s", line = get_next_line(fd2));
+	printf("%s", line = get_next_line(fd1));
+	printf("%s", line = get_next_line(fd0));
+	printf("%s", line = get_next_line(fd2));
+	printf("%s", line = get_next_line(fd1));
+	printf("%s\n", line = get_next_line(fd0));
+	printf("%s", line = get_next_line(fd2));
+	printf("%s", line = get_next_line(fd1));
+	printf("%s", line = get_next_line(fd0));
+	free(line);
 	close(fd2);
-
-
-	printf("BUFFER_SIZE=%d\n", BUFFER_SIZE);
-	int fd3 = open("test1", O_RDONLY);
-	int fd4 = open("test2", O_RDONLY);
-	i = 0;
-
-
-
-	printf("%s\n", get_next_line(fd3));
-	printf("%s\n", get_next_line(fd4));
-	printf("%s\n", get_next_line(fd3));
-	printf("%s\n", get_next_line(fd4));
-	printf("%s\n", get_next_line(fd3));
-	printf("%s\n", get_next_line(fd4));
-
-	close(fd3);
-	close(fd4);
+	free(line);
+	close(fd1);
+	free(line);
+	close(fd0);
 }
