4eb03ada7b548a9325825153c524eeb7ea1a817c
fran-byte <fran-byte@hotmail.com>
2025-01-02 21:34:54 +0100

    update

--- 42_Madrid/milestone_2/so_long/so_long_project/src/data_move.c
+++ 42_Madrid/milestone_2/so_long/so_long_project/src/data_move.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 11:25:56 by frromero          #+#    #+#             */
-/*   Updated: 2025/01/02 21:31:45 by frromero         ###   ########.fr       */
+/*   Updated: 2025/01/02 21:34:42 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,41 @@
 #include <stdio.h>
 void move_player(t_game *game, int dx, int dy)
 {
-    int new_x;
-    int new_y;
+	int new_x;
+	int new_y;
 
-    new_x = game->map.player_x + dx;
-    new_y = game->map.player_y + dy;
-    if (game->map.grid[new_y][new_x] != '1')
-    {
-        if (game->map.grid[new_y][new_x] == 'C')
-            game->map.collectibles--;
-        if (game->map.grid[game->map.player_y][game->map.player_x] != 'E')
-            game->map.grid[game->map.player_y][game->map.player_x] = '0';
-        if (game->map.grid[new_y][new_x] == 'E')
-        {
-            if (game->map.collectibles == 0)
-            {
-                game->game_over = 1; // GAME OVER
-                printf("Juego terminado! Has recogido todos los coleccionables.\n");
-                game->moves++;
-                exit_game(game);
-                return;
-            }
-            else
-            {
-                printf("* * * Aún faltan coleccionables para salir.\n");
-                game->map.grid[new_y][new_x] = 'E';
-                game->map.player_x = new_x;
-                game->map.player_y = new_y;
-                game->moves++;
-                return;
-            }
-        }
-        game->map.grid[new_y][new_x] = 'P';
-        game->map.player_x = new_x;
-        game->map.player_y = new_y;
-        game->moves++;
-    }
-    printf("Jugador en (%d, %d), Coleccionables: %d\n", game->map.player_x, game->map.player_y, game->map.collectibles);
+	new_x = game->map.player_x + dx;
+	new_y = game->map.player_y + dy;
+	if (game->map.grid[new_y][new_x] != '1')
+	{
+		if (game->map.grid[new_y][new_x] == 'C')
+			game->map.collectibles--;
+		if (game->map.grid[game->map.player_y][game->map.player_x] != 'E')
+			game->map.grid[game->map.player_y][game->map.player_x] = '0';
+		if (game->map.grid[new_y][new_x] == 'E')
+		{
+			if (game->map.collectibles == 0)
+			{
+				game->game_over = 1; // GAME OVER
+				printf("Juego terminado! Has recogido todos los coleccionables.\n");
+				game->moves++;
+				exit_game(game);
+				return;
+			}
+			else
+			{
+				printf("* * * Aún faltan coleccionables para salir.\n");
+				game->map.grid[new_y][new_x] = 'E';
+				game->map.player_x = new_x;
+				game->map.player_y = new_y;
+				game->moves++;
+				return;
+			}
+		}
+		game->map.grid[new_y][new_x] = 'P';
+		game->map.player_x = new_x;
+		game->map.player_y = new_y;
+		game->moves++;
+	}
+	printf("Jugador en (%d, %d), Coleccionables: %d\n", game->map.player_x, game->map.player_y, game->map.collectibles);
 }
-
