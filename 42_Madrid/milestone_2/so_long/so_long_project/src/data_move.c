6e296668bfbebf70b8a00fd443d2afb59085367c
fran-byte <fran-byte@hotmail.com>
2025-01-02 20:20:18 +0100

    update data_move

--- 42_Madrid/milestone_2/so_long/so_long_project/src/data_move.c
+++ 42_Madrid/milestone_2/so_long/so_long_project/src/data_move.c
@@ -6,7 +6,7 @@
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/27 11:25:56 by frromero          #+#    #+#             */
-/*   Updated: 2025/01/02 19:48:29 by frromero         ###   ########.fr       */
+/*   Updated: 2025/01/02 20:19:34 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,32 +25,20 @@ void move_player(t_game *game, int dx, int dy)
 	// Si el movimiento no es hacia una pared
 	if (game->map.grid[new_y][new_x] != '1')
 	{
-		// Si el jugador pasa por un coleccionable, decrementa
 		if (game->map.grid[new_y][new_x] == 'C')
 			game->map.collectibles--;
 
 		// Borra la antigua posición del jugador solo si no es la salida
-		//if (game->map.grid[game->map.player_y][game->map.player_x] != game->map.grid[game->map.exit_y][game->map.exit_x])
-		//{
-			game->map.grid[game->map.player_y][game->map.player_x] = '0'; // Borra la antigua posición
-		//}
+		//if (game->map.grid[new_y][new_x] == 'E')
+	//		game->map.exit_x = new_x, game->map.exit_y = new_y;// Borra la antigua posición
+	//	else
+			game->map.grid[game->map.player_y][game->map.player_x] = '0';
 
 		// Asigna la nueva posición al jugador
 		game->map.grid[new_y][new_x] = 'P'; // Coloca al jugador en la nueva celda
 		game->map.player_x = new_x;
 		game->map.player_y = new_y;
 		game->moves++;
-
-		// Renderiza la nueva posición del jugador
-		mlx_put_image_to_window(game->mlx, game->window, game->sprites.player,
-								new_x * TILE_SIZE, new_y * TILE_SIZE);
-
-		// Siempre renderiza la salida en su lugar
-		if (game->map.grid[new_y][new_x] != 'E') // Solo si el jugador no está en la salida
-		{
-			mlx_put_image_to_window(game->mlx, game->window, game->sprites.exit,
-									game->map.exit_x * TILE_SIZE, game->map.exit_y * TILE_SIZE);
-		}
 
 		// Si el jugador ha llegado a la salida
 		if (game->map.grid[new_y][new_x] == 'E')
