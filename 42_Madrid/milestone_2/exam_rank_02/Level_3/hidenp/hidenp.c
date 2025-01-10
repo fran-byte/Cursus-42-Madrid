d839ba95c60b5568dced59a20742adfde7bd3496
fran-byte <fran-byte@hotmail.com>
2025-01-10 15:43:46 +0100

    Update hidenp.c

--- 42_Madrid/milestone_2/exam_rank_02/Level_3/hidenp/hidenp.c
+++ 42_Madrid/milestone_2/exam_rank_02/Level_3/hidenp/hidenp.c
@@ -5,41 +5,37 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/06 16:37:38 by frromero          #+#    #+#             */
-/*   Updated: 2025/01/06 17:18:22 by frromero         ###   ########.fr       */
+/*   Created: 2025/01/10 15:43:00 by frromero          #+#    #+#             */
+/*   Updated: 2025/01/10 15:43:00 by frromero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include <unistd.h>
+#include<unistd.h>
 
 int	main(int argc, char **argv)
 {
-	int j = 0;
 	int i = 0;
+	int j = 0;
 
 	if(argc == 3)
 	{
-		while(argv[1][i] && argv[2][j])
+		while(argv[1][i] != '\0')
 		{
-			if(argv[1][i] == argv[2][j])
+			while(argv[2][j] != '\0')
 			{
-				if(argv[1][i + 1] == '\0')
+				if(argv[1][i] == argv[2][j])
+					break;
+				j++;
+			}
+			i++;
+			if(argv[2][j] == '\0' && argv[1][i] == '\0')
 				{
-					write(1, "1", 1);
+					write(1, "0", 1);
 					write(1, "\n", 1);
 					return(0);
 				}
-				j++;
-				i++;
-			}
-			else
-				j++;
-			if(argv[1][i] && argv[2][j + 1] == '\0')
-				break;
 		}
-		write(1, "0", 1);
-		write(1, "\n", 1);
-		return(0);
+		write(1, "1", 1);
 	}
 	write(1, "\n", 1);
 	return(0);
