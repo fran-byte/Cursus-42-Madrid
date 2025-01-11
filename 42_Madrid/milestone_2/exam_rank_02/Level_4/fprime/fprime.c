eef00be93297c3ff8aba52f8454bd1c00a08a99e
fran-byte <fran-byte@hotmail.com>
2025-01-11 11:34:09 +0100

    up

---
+++ 42_Madrid/milestone_2/exam_rank_02/Level_4/fprime/fprime.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fprime.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: frromero <frromero@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/11 11:33:37 by frromero          #+#    #+#             */
+/*   Updated: 2025/01/11 11:33:41 by frromero         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int	main(int argc, char *argv[])
+{
+	int	i;
+	int	number;
+
+	if (argc == 2)
+	{
+		i = 1;
+		number = atoi(argv[1]);
+		if (number == 1)
+			printf("1");
+		while (number >= ++i)
+		{
+			if (number % i == 0)
+			{
+				printf("%d", i);
+				if (number == i)
+					break ;
+				printf("*");
+				number /= i;
+				i = 1;
+			}
+		}
+	}
+	printf("\n");
+	return (0);
+}
