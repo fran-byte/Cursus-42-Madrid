dcf3d4dc82c46e5aae950de333b6c84f2c4d23e8
fran-byte <fran-byte@hotmail.com>
2025-01-12 22:28:16 +0100

    Update sort_list.c

--- 42_Madrid/milestone_2/exam_rank_02/Level_4/sort_list/sort_list.c
+++ 42_Madrid/milestone_2/exam_rank_02/Level_4/sort_list/sort_list.c
@@ -18,17 +18,17 @@ t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 
 	start = lst;
 
-	while (lst != NULL && lst->next != NULL)
+	while (lst != NULL && lst->next != NULL) //Mientras tengamos nodo y el nodo siguiente a comparar
 	{
-		if ((*cmp)(lst->data, lst->next->data) == 0)
-		{
+		if ((*cmp)(lst->data, lst->next->data) == 0) // Si no están en orden
+		{  // Intercambiamos los valores
 			swap = lst->data;
 			lst->data = lst->next->data;
 			lst->next->data = swap;
-			lst = start;
+			lst = start;  // reiniciamos la lista para empezar de nuevo
 		}
 		else
 			lst = lst->next;
 	}
-	return (start);
+	return (start); // devolvemos el puntero al primer nodo de la lista.
 }
