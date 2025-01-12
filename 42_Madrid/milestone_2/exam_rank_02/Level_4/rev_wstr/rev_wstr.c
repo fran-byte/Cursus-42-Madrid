3ce6b22706f6a7432326066b6e34392840a58276
fran-byte <fran-byte@hotmail.com>
2025-01-12 12:27:21 +0100

    Update rev_wstr.c

--- 42_Madrid/milestone_2/exam_rank_02/Level_4/rev_wstr/rev_wstr.c
+++ 42_Madrid/milestone_2/exam_rank_02/Level_4/rev_wstr/rev_wstr.c
@@ -10,24 +10,25 @@ int main(int argc, char **argv)
 	{
 		while(argv[1][i] != '\0')
 			i++;
+		i--;
 		while(i >= 0)
 		{
-			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')
+			while(argv[1][i] == ' ' || argv[1][i] == '\t')
 				i--;
 			end = i;
 			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')
 				i--;
 			start = i + 1;
-			int  flag;
-			flag = start;
 			while(start <= end)
 			{
 				write (1, &argv[1][start],1);
 				start++;
 			}
-			if (flag !=0)
+			if (i >=0) // no estamos en la primera posición
 				write(1, " ", 1);
 		}
 	}
 	write(1, "\n", 1);
+	return (0);
 }
+
