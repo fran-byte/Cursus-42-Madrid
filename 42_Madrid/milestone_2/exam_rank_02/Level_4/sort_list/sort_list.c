3b3b2c5f98a75b6868ae97221406581b3ab086da
fran-byte <fran-byte@hotmail.com>
2025-01-13 19:36:54 +0100

    Update sort_list.c

--- 42_Madrid/milestone_2/exam_rank_02/Level_4/sort_list/sort_list.c
+++ 42_Madrid/milestone_2/exam_rank_02/Level_4/sort_list/sort_list.c
@@ -1,15 +1,6 @@
 #include <stdlib.h>
 
-
-typedef struct s_list t_list;
-
-struct s_list
-{
-	int		data;
-	t_list	*next;
-};
-
-
+#include "list.h"
 
 t_list *sort_list(t_list *lst, int (*cmp)(int, int))
 {
